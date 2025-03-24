c4397876154fa2fa8c4b1578ff0309f21bbc4192
Axel Damart <axel.damart@gmail.com>
2025-03-24 15:16:43 +0100

    fix: message in english

--- backend/src/DTO/Bar/DeleteBarDTO.php
+++ backend/src/DTO/Bar/DeleteBarDTO.php
@@ -6,8 +6,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 readonly class DeleteBarDTO
 {
-   #[Assert\NotBlank(message: "L'id du bar est requis.")]
-   #[Assert\Type(type: 'integer', message: "L'id du bar doit être un nombre entier.")]
+   #[Assert\NotBlank(message: "Bar is is required")]
+   #[Assert\Type(type: 'integer', message: "Bar id must be an integer")]
    public int $id;
 
    public function __construct(int $id)
