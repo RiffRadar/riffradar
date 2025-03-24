c8852f668b48c14f3ad55b1914ef7f1732af6cf6
Axel Damart <axel.damart@gmail.com>
2025-03-24 15:19:09 +0100

    feat: created band controller

---
+++ backend/src/Controller/BandController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/bands')]
+final class BandController extends AbstractController {}
